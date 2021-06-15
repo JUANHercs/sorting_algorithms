0985369409e6615a975648833e0c256f9b78f3e3
“JUANHercs” <juan.hercs@gmail.com>
2021-06-15 18:47:15 -0500

    header

---
+++ sort.h
@@ -0,0 +1,28 @@
+#ifdef _SORT_H_
+#define _SORT_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - Double linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previus elemnt of yhe link
+ * @next: Pointer to the next element of the link
+ **/
+
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+/* prototypes from given print functions*/
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+
+
+#endif /* _SORT_H_ */
