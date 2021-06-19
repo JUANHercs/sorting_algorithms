2915751e737a5cadb025e2ac552ea6dd856de639
“JUANHercs” <juan.hercs@gmail.com>
2021-06-19 13:14:48 -0500

    upload_function

--- sort.h
+++ sort.h
@@ -1,8 +1,9 @@
-#ifdef _SORT_H_
+#ifndef _SORT_H_
 #define _SORT_H_
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 /**
  * struct listint_s - Double linked list node
@@ -23,6 +24,9 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
+/* function protoypes */
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
 
 #endif /* _SORT_H_ */
